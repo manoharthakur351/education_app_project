fde599eae61c4a7a6c8002a8b71752d9f1e51f3c
Manohar <tsmile2459@gmail.com>
2021-05-22 14:35:48 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,62 @@
-<ActivityManager>:
-	HomeActivity:
-		MDLabel:
-			text:'ooo'*467
+import kivymd
+import webbrowser
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.navigationdrawer import NavigationLayout
+from kivymd.icon_definitions import md_icons
+from kivymd.toast import toast
+from kivymd.uix.imagelist import SmartTileWithLabel
+from kivymd.uix.list import OneLineListItem,\
+							TwoLineListItem,\
+							ThreeLineListItem,\
+							OneLineIconListItem,\
+							MDList
+
+class TextWaliTile (SmartTileWithLabel):
+	pass
+
+class MainNavigator (NavigationLayout):
+	pass
+
+class ActivityManager (ScreenManager):
+	pass
+
+class HomeActivity (Screen):
+	pass
+
+class Scroll (ScrollView):
+	pass
+
+class ListView (MDList):
+	
+	pass
+
+class OneLineIcon(OneLineIconListItem):
+	text = '1234567890'
+	def on_press(self):
+		MyApp().toastit(self.text)
+	pass
+
+
+class MyApp (MDApp):
+	def webit (self,link = 'https://www.google.com/'):
+		webbrowser.open(link)
+
+	def toastit (self,a='No function yet'):
+		toast(a)
+		pass
+
+	def build(self):
+		
+		Builder.load_file('main.kv')
+		a = ActivityManager()
+		mainNavigator = MainNavigator()
+		
+		return mainNavigator
+
+
+if __name__== '__main__':
+	MyApp().run()
+
